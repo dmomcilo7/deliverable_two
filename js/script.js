8ac9dffd28c6b827a7f3dcf1b256d6d29cd4abc0
Dusan <dmomcilo@hotmail.com>
2022-01-14 06:20:08 -0500

    Second commit to deliverable two

--- js/script.js
+++ js/script.js
@@ -3,24 +3,23 @@ let gameNumber = 0;
 let par = 3;
 let score = 0;
 
-prompt("How many family members?");
 
-//user = prompt('Welcome to GC mini-golf! What is your name?', user);
-//gameNumber = prompt(`Hi, ${user}! Would you like to play 3 or 6 holes?`, gameNumber);
-//par = par * gameNum;
 
-//for (let i = 0; i < gameNumber; i++) {
-  //  let putts = 0;   
-    //putts = prompt(`How many putts for hole ${index + 1}? (par: 3)`, putts);
-    //score += Number(putts);
-    //console.log('putts', putts)
-    //console.log('score', score)
-//}
+user = prompt('Welcome to GC mini-golf! What is your name?', user);
+gameNumber = prompt(`Hi, ${user}! Would you like to play 3 or 6 holes?`, gameNumber);
+par = par * gameNumber;
 
-//if (score === par) {
-//    console.log(`Good game, ${user}. Your total par was: 0.`)
-//} else if (score < par) {
-//    console.log(`Great job, ${user}! Your total par was: -${par - score}`)
-//} else {
-//    console.log(`Nice try, ${user}! Your total par was: +${score - par}`)
-//}
+for (let i = 0; i < gameNumber; i++) {
+   let putts = 0;   
+    putts = prompt(`How many putts for hole ${i + 1}? (par: 3)`, putts);
+    score += Number(putts);
+    console.log('putts', putts)
+    console.log('score', score)
+}
+if (score === par) {
+    console.log(`Good game, ${user}. Your total par was: 0.`)
+} else if (score < par) {
+    console.log(`Great job, ${user}! Your total par was: -${par - score}`)
+} else {
+   console.log(`Nice try, ${user}! Your total par was: +${score - par}`)
+}
