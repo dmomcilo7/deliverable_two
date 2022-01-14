36e8f5b909dc1fd956035e1813d2428d9bb1dfe0
Dusan <dmomcilo@hotmail.com>
2022-01-14 06:15:45 -0500

    First commit for deliverable two

---
+++ js/script.js
@@ -0,0 +1,26 @@
+let user = '';
+let gameNumber = 0;
+let par = 3;
+let score = 0;
+
+prompt("How many family members?");
+
+//user = prompt('Welcome to GC mini-golf! What is your name?', user);
+//gameNumber = prompt(`Hi, ${user}! Would you like to play 3 or 6 holes?`, gameNumber);
+//par = par * gameNum;
+
+//for (let i = 0; i < gameNumber; i++) {
+  //  let putts = 0;   
+    //putts = prompt(`How many putts for hole ${index + 1}? (par: 3)`, putts);
+    //score += Number(putts);
+    //console.log('putts', putts)
+    //console.log('score', score)
+//}
+
+//if (score === par) {
+//    console.log(`Good game, ${user}. Your total par was: 0.`)
+//} else if (score < par) {
+//    console.log(`Great job, ${user}! Your total par was: -${par - score}`)
+//} else {
+//    console.log(`Nice try, ${user}! Your total par was: +${score - par}`)
+//}
